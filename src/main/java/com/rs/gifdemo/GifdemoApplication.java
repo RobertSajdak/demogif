745be3341670ce6e4a24a6c1c37b960702bc412f
Robert <robert.sajdak@onet.eu>
2019-10-12 16:28:00 +0200

    first commit

---
+++ src/main/java/com/rs/gifdemo/GifdemoApplication.java
@@ -0,0 +1,13 @@
+package com.rs.gifdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GifdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GifdemoApplication.class, args);
+	}
+
+}
